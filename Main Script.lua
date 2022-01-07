af419df8f6b90e379a025a89a40a09d1ae3da9a5
358x3 <54907369+358x33@users.noreply.github.com>
2022-01-07 11:46:04 +0100

    Update Main Script.lua

--- Main Script.lua
+++ Main Script.lua
@@ -1,13 +1,10 @@
+repeat wait() until game:IsLoaded()
 local PlaceId = game.PlaceId
 local PlayerService = game:GetService("Players")
 local plr = PlayerService.LocalPlayer
 
 if PlaceId == 8438695556 then
-    if not game:IsLoaded() then
-        game.Loaded:Wait()
-    else
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/358x33/JustAHub/main/FarmUi.lua"))()
-    end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/358x33/JustAHub/main/FarmUi.lua"))()
 else
     plr:kick("Wrong game")
 end
