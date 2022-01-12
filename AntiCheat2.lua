c4469d0f4057f80011e4962f5177208aefa589d4
358x3 <54907369+358x33@users.noreply.github.com>
2022-01-12 20:04:49 +0100

    Add files via upload

---
+++ AntiCheat2.lua
@@ -0,0 +1,31 @@
+local hook = game:GetService("Players").LocalPlayer.Kick;
+local mt = getrawmetatable(game:GetService("Players").LocalPlayer)
+local psuedoEnv = {
+    ["__index"] = mt.__index,
+    ["__namecall"] = mt.__namecall
+}
+local ReturnFunc = function()end
+
+print("Ch0nky#8948 - 579306070040641546 AntiKick") -- remove this if you want, just dont claim it as yours ty
+setreadonly(mt, false)
+mt.__namecall = newcclosure(function(self, Index, ...)
+    local NameCallMethod = getnamecallmethod()
+    if type(NameCallMethod) == "string" then
+        if NameCallMethod == "Ban" or NameCallMethod == "ban" then
+            print("Prevented Kick At " .. tostring(os.time()))
+            return ReturnFunc
+        end
+    end
+    return psuedoEnv.__namecall(self, Index, ...)
+end)
+
+mt.__index = newcclosure(function(self, Index, ...)
+    if type(Index) == "string" then
+        if Index == "Ban" or Index == "ban" then
+            print("Prevented Kick At " .. tostring(os.time()))
+            return ReturnFunc
+        end
+    end
+    return psuedoEnv.__index(self, Index, ...)
+end)
+setreadonly(mt, true)
